3979b877d98c208dd8b1e60cc77d16e3bc0b81a0
RyuyaKato <177389012+kato3infra@users.noreply.github.com>
2024-08-11 13:55:57 +0000

    [Finish] 15章

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about', to: 'homes#about', as: 'about'
 end
